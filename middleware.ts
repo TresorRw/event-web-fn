c2c25f70851412e1428e4ba5f544e692391d81cd
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-29 01:15:03 +0200

    Added clerk middleware to handle different scenariors and userButton

--- middleware.ts
+++ middleware.ts
@@ -2,23 +2,21 @@ import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 export default authMiddleware({
-  publicRoutes: ['/'],
-  afterAuth: (auth, req, evt) => {
-    // Handle users who are not authenticated
-    if (!auth.user && !auth.isPublicRoute) {
-      return redirectToSignIn({returnBackUrl: req.url})
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
     }
-
-    // if the user is logged in and trying to access a protected route
-    if (auth.user && auth.isPublicRoute) {
-      return NextResponse.next();
+    
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next()
     }
-
     // Allow users visiting public routes to access them
     return NextResponse.next();
   }
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
