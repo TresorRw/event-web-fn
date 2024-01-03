1279ca57cb11214f8a738f7695f6e8cfb030f4b7
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-03 17:26:11 +0200

    Sign in and sign up pages

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,7 +3,23 @@ const SignInPage = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Sign In</h1>
+        <div className="card w-full bg-base-100 shadow-xl">
+            <div className="card-body items-center">
+              <h2 className="card-title text-2xl border-b-2">TresEvents</h2>
+              <p>Sign into your account</p>
+              <form className="mt-5">
+                <div className="label">
+                  <span className="label-text-alt">Email</span>
+                </div>
+                <input type="email" className="input input-sm input-bordered w-full" />
+                <div className="label">
+                  <span className="label-text-alt">Password</span>
+                </div>
+                <input type="password" className="input input-sm input-bordered w-full" />
+                <button type="submit" className="btn btn-sm mt-5 w-full btn-primary">Sign In</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,7 +3,27 @@ const SignUpPage = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Sign Up</h1>
+          <div className="card w-full bg-base-100 shadow-xl">
+            <div className="card-body items-center">
+              <h2 className="card-title text-2xl border-b-2">TresEvents</h2>
+              <p>Create your account to get started</p>
+              <form className="mt-5">
+                <div className="label">
+                  <span className="label-text-alt">Email</span>
+                </div>
+                <input type="email" className="input input-sm input-bordered w-full" />
+                <div className="label">
+                  <span className="label-text-alt">Names</span>
+                </div>
+                <input type="text" className="input input-sm input-bordered w-full" />
+                <div className="label">
+                  <span className="label-text-alt">Password</span>
+                </div>
+                <input type="password" className="input input-sm input-bordered w-full" />
+                <button type="submit" className="btn btn-sm mt-5 w-full btn-primary">Create account</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
